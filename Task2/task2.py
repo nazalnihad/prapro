98a25dc19f0333b756c92ec55e929096ff0a2295
Nazal-nihad <nazalnihad17@gmail.com>
2024-04-07 15:33:15 +0530

    passing data type of the array

--- Task2/task2.py
+++ Task2/task2.py
@@ -19,9 +19,9 @@ finish = time.time()
 print(f"creation time = {finish-start}s\n")
 print(f"created array \n {array[0][:10]}\n")
 
-
 array_to_bytes = array.tobytes()
 print(f"converted to bytes \n {array_to_bytes[:10]}\n")
 
-recreated = np.frombuffer(array_to_bytes).reshape(1000,1000)
+arr_type = array.dtype
+recreated = np.frombuffer(array_to_bytes,dtype=arr_type,).reshape(1000,1000)
 print(f"recreated from bytes \n {recreated[0][0:10]}")
