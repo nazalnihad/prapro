a36c03b04f6ea158198b3f35789736482dd08ed1
Nazal-nihad <nazalnihad17@gmail.com>
2024-04-07 14:00:28 +0530

    questions commented

--- Task2/task2.py
+++ Task2/task2.py
@@ -1,3 +1,15 @@
+"""
+Create a 1000 x 1000 random numpy array
+Measure how long the creation takes
+Convert the array into bytes
+Recreate the array from the bytes
+
+References:
+time package
+numpy package
+np.frombuffer
+"""
+
 import numpy as np
 import time
 
